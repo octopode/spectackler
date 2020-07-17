1f1764d2b23457cb9237ad4d3ccd458585262e26
Cubette <cubette@Thalassa.local>
2020-07-17 14:20:41 -0700

    test terminals

--- 20200618_iscotest.py
+++ 20200618_iscotest.py
@@ -8,8 +8,8 @@ import isco260D as isco
 from time import sleep
 
 def gg2psi(gg_str):
-	"get PSI value from G& get all status string"
-	return float(gg_str.decode("utf-8").split('=')[1].split(',')[0]) / 5
+    "get PSI value from G& get all status string"
+    return float(gg_str.decode("utf-8").split('=')[1].split(',')[0]) / 5
 
 pump = isco.ISCOController(port="/dev/cu.usbserial-FTV5C58R1",source=0,dest=1)
 
@@ -17,9 +17,9 @@ pump.remote()
 print(pump.gg())
 
 while True:
-	cmd = input("pump.")
-	try:
-		ret = eval("pump.{}".format(cmd))
-		print(ret)
-	except:
-		pump.disconnect()
+    cmd = input("pump.")
+    try:
+        ret = eval("pump.{}".format(cmd))
+        print(ret)
+    except:
+        pump.disconnect()
