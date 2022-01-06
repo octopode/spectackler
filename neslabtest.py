5749a7bf7300dddf54fcb2ce1bd8734b17d16019
octopode <jwinnikoff@ucsc.edu>
2022-01-05 16:38:43 -0800

    ported to py3

--- neslabtest.py
+++ neslabtest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 initialize Neslab water bath, then provide user with an interactive console for debugging
@@ -8,11 +8,11 @@ import neslabrte as rte
 import traceback
 import __future__
 
-bath = rte.NeslabController(port="/dev/cu.usbserial-FT4IVKAO0")
+bath = rte.NeslabController(port="/dev/cu.usbserial-FT4IVKAO1")
 #bath = rte.NeslabController(port="/dev/cu.Bluetooth-Incoming-Port")
 
 while True:
-	cmd = raw_input("bath.")
+	cmd = input("bath.")
 	try:
 	    print(eval("bath.{}".format(cmd)))
 	except:
